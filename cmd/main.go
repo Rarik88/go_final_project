6d27deb2f4ade6e56a3780fe275fbc740e9a27b7
Alex Nuka <alex.nuka@yandex.ru>
2024-04-30 18:14:00 +0300

    Доработка проекта v1

--- cmd/main.go
+++ cmd/main.go
@@ -1,17 +1,23 @@
 package main
 
 import (
-	_ "github.com/mattn/go-sqlite3"
 	app "github/Rarik88/go_final_project"
-	"github/Rarik88/go_final_project/pkg/api"
 	con "github/Rarik88/go_final_project/pkg/const"
 	hl "github/Rarik88/go_final_project/pkg/handler"
 	"github/Rarik88/go_final_project/pkg/hub"
+	api "github/Rarik88/go_final_project/pkg/service"
 	"log"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
+// добавлена обработка ощибок в Дб
 func main() {
 	db := hub.Sqlite(con.DB_NAME_SET)
+	if db == nil {
+		log.Fatalf("Не удалось открыть соединение с базой данных")
+	}
+	defer db.Close()
 	repo := hub.NewDB(db)
 	service := api.NewApi(repo)
 	handler := hl.NewHandler(service)
